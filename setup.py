f93ed86b25815881aaf4026a941e3ec9dc9ce42b
pwalker91 <pwalker@csumb.edu>
2015-02-05 15:15:00 -0800

    setup.py now works correctly

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.5.0',
 
     description='A easier-to-use, academic wrapper class for the PIL module',
     long_description=long_description,
@@ -55,8 +55,8 @@ setup(
         #'Programming Language :: Python :: 2',
         #'Programming Language :: Python :: 2.6',
         #'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
@@ -66,7 +66,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=['SimpleImage'],
 
     # List run-time dependencies here.  These will be installed by pip when your
     # project is installed. For an analysis of "install_requires" vs pip's
